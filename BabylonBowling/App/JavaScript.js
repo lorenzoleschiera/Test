b74474fe5f4b5256d335297f41a55d71cf3514e9
Jessica <bee90@live.it>
2018-12-15 17:15:19 +0100

    Update JavaScript.js

--- BabylonBowling/App/JavaScript.js
+++ BabylonBowling/App/JavaScript.js
@@ -386,103 +386,508 @@ function animation() {
 
     moveRightLeg_keys.push({
         frame: 0,
-        value: new BABYLON.Quaternion.RotationAxis(axis, -0.28).toEulerAngles()
+        value: new BABYLON.Quaternion.RotationAxis(axis, 0.90).toEulerAngles()
     });
 
     moveRightLeg_keys.push({
         frame: 1 * frameRate,
-        value: new BABYLON.Quaternion.RotationAxis(axis, 0).toEulerAngles()
+        value: new BABYLON.Quaternion.RotationAxis(axis, 0.40).toEulerAngles()
     });
 
     moveRightLeg_keys.push({
         frame: 2 * frameRate,
-        value: new BABYLON.Quaternion.RotationAxis(axis, 0.35).toEulerAngles()
+        value: new BABYLON.Quaternion.RotationAxis(axis, 0.20).toEulerAngles()
     });
 
     moveRightLeg_keys.push({
         frame: 3 * frameRate,
-        value: new BABYLON.Quaternion.RotationAxis(axis, 0.87).toEulerAngles()
+        value: new BABYLON.Quaternion.RotationAxis(axis, 0.00).toEulerAngles()
     });
 
     moveRightLeg_keys.push({
         frame: 4 * frameRate,
-        value: new BABYLON.Quaternion.RotationAxis(axis, 0.59).toEulerAngles()
+        value: new BABYLON.Quaternion.RotationAxis(axis, -0.40).toEulerAngles()
     });
 
     moveRightLeg_keys.push({
         frame: 5 * frameRate,
-        value: new BABYLON.Quaternion.RotationAxis(axis, 0.59).toEulerAngles()
+        value: new BABYLON.Quaternion.RotationAxis(axis, -0.20).toEulerAngles()
     });
 
     moveRightLeg_keys.push({
         frame: 6 * frameRate,
-        value: new BABYLON.Quaternion.RotationAxis(axis, 0.09).toEulerAngles()
+        value: new BABYLON.Quaternion.RotationAxis(axis, 0.20).toEulerAngles()
     });
 
     moveRightLeg_keys.push({
         frame: 7 * frameRate,
-        value: new BABYLON.Quaternion.RotationAxis(axis, -0.35).toEulerAngles()
+        value: new BABYLON.Quaternion.RotationAxis(axis, 0.40).toEulerAngles()
     });
     moveRightLeg_keys.push({
         frame: 8 * frameRate,
-        value: new BABYLON.Quaternion.RotationAxis(axis, -0.28).toEulerAngles()
+        value: new BABYLON.Quaternion.RotationAxis(axis, 0.90).toEulerAngles()
     });
-
-
 
     var moveLeftLeg = new BABYLON.Animation("moveLeftLeg", "rotation", 5, BABYLON.Animation.ANIMATIONTYPE_VECTOR3, BABYLON.Animation.ANIMATIONLOOPMODE_CYCLE);
 
     var moveLeftLeg_keys = [];
     moveLeftLeg_keys.push({
         frame: 0 * frameRate,
-        value: new BABYLON.Quaternion.RotationAxis(axis, 0.59).toEulerAngles()
+        value: new BABYLON.Quaternion.RotationAxis(axis, -0.40).toEulerAngles()
     });
 
     moveLeftLeg_keys.push({
         frame: 1 * frameRate,
-        value: new BABYLON.Quaternion.RotationAxis(axis, 0.59).toEulerAngles()
+        value: new BABYLON.Quaternion.RotationAxis(axis, -0.20).toEulerAngles()
     });
 
     moveLeftLeg_keys.push({
         frame: 2 * frameRate,
-        value: new BABYLON.Quaternion.RotationAxis(axis, 0.09).toEulerAngles()
+        value: new BABYLON.Quaternion.RotationAxis(axis, 0.20).toEulerAngles()
     });
 
     moveLeftLeg_keys.push({
         frame: 3 * frameRate,
-        value: new BABYLON.Quaternion.RotationAxis(axis, -0.35).toEulerAngles()
+        value: new BABYLON.Quaternion.RotationAxis(axis, 0.40).toEulerAngles()
     });
 
     moveLeftLeg_keys.push({
         frame: 4 * frameRate,
-        value: new BABYLON.Quaternion.RotationAxis(axis, -0.28).toEulerAngles()
+        value: new BABYLON.Quaternion.RotationAxis(axis, 0.90).toEulerAngles()
     });
 
     moveLeftLeg_keys.push({
         frame: 5 * frameRate,
-        value: new BABYLON.Quaternion.RotationAxis(axis, 0).toEulerAngles()
+        value: new BABYLON.Quaternion.RotationAxis(axis, 0.40).toEulerAngles()
     });
 
     moveLeftLeg_keys.push({
         frame: 6 * frameRate,
-        value: new BABYLON.Quaternion.RotationAxis(axis, 0.35).toEulerAngles()
+        value: new BABYLON.Quaternion.RotationAxis(axis, 0.20).toEulerAngles()
     });
 
     moveLeftLeg_keys.push({
         frame: 7 * frameRate,
-        value: new BABYLON.Quaternion.RotationAxis(axis, 0.87).toEulerAngles()
+        value: new BABYLON.Quaternion.RotationAxis(axis, 0.00).toEulerAngles()
     });
+
     moveLeftLeg_keys.push({
         frame: 8 * frameRate,
-        value: new BABYLON.Quaternion.RotationAxis(axis, 0.59).toEulerAngles()
+        value: new BABYLON.Quaternion.RotationAxis(axis, -0.40).toEulerAngles()
     });
+
+
+    var moveRightLegDown_keys = [];
+
+    moveRightLegDown_keys.push({
+        frame: 0,
+        value: new BABYLON.Quaternion.RotationAxis(axis, -0.70).toEulerAngles()
+    });
+
+    moveRightLegDown_keys.push({
+        frame: 1 * frameRate,
+        value: new BABYLON.Quaternion.RotationAxis(axis, -0.60).toEulerAngles()
+    });
+
+    moveRightLegDown_keys.push({
+        frame: 2 * frameRate,
+        value: new BABYLON.Quaternion.RotationAxis(axis, -0.40).toEulerAngles()
+    });
+
+    moveRightLegDown_keys.push({
+        frame: 3 * frameRate,
+        value: new BABYLON.Quaternion.RotationAxis(axis, 0.00).toEulerAngles()
+    });
+
+    moveRightLegDown_keys.push({
+        frame: 4 * frameRate,
+        value: new BABYLON.Quaternion.RotationAxis(axis, -0.40).toEulerAngles()
+    });
+
+    moveRightLegDown_keys.push({
+        frame: 5 * frameRate,
+        value: new BABYLON.Quaternion.RotationAxis(axis, -0.60).toEulerAngles()
+    });
+
+    moveRightLegDown_keys.push({
+        frame: 6 * frameRate,
+        value: new BABYLON.Quaternion.RotationAxis(axis, -0.70).toEulerAngles()
+    });
+
+    moveRightLegDown_keys.push({
+        frame: 7 * frameRate,
+        value: new BABYLON.Quaternion.RotationAxis(axis, -1.00).toEulerAngles()
+    });
+    moveRightLegDown_keys.push({
+        frame: 8 * frameRate,
+        value: new BABYLON.Quaternion.RotationAxis(axis, -0.70).toEulerAngles()
+    });
+    var moveRightLegDown = new BABYLON.Animation("moveRightLegDown", "rotation", 5, BABYLON.Animation.ANIMATIONTYPE_VECTOR3, BABYLON.Animation.ANIMATIONLOOPMODE_CYCLE);
+
+
+    var moveLeftLegDown_keys = [];
+    moveLeftLegDown_keys.push({
+        frame: 0 * frameRate,
+        value: new BABYLON.Quaternion.RotationAxis(axis, 0.00).toEulerAngles()
+    });
+
+    moveLeftLegDown_keys.push({
+        frame: 1 * frameRate,
+        value: new BABYLON.Quaternion.RotationAxis(axis, -0.40).toEulerAngles()
+    });
+
+    moveLeftLegDown_keys.push({
+        frame: 2 * frameRate,
+        value: new BABYLON.Quaternion.RotationAxis(axis, -0.60).toEulerAngles()
+    });
+
+    moveLeftLegDown_keys.push({
+        frame: 3 * frameRate,
+        value: new BABYLON.Quaternion.RotationAxis(axis, -0.70).toEulerAngles()
+    });
+
+    moveLeftLegDown_keys.push({
+        frame: 4 * frameRate,
+        value: new BABYLON.Quaternion.RotationAxis(axis, -1.00).toEulerAngles()
+    });
+
+    moveLeftLegDown_keys.push({
+        frame: 5 * frameRate,
+        value: new BABYLON.Quaternion.RotationAxis(axis, -0.70).toEulerAngles()
+    });
+
+    moveLeftLegDown_keys.push({
+        frame: 6 * frameRate,
+        value: new BABYLON.Quaternion.RotationAxis(axis, -0.60).toEulerAngles()
+    });
+
+    moveLeftLegDown_keys.push({
+        frame: 7 * frameRate,
+        value: new BABYLON.Quaternion.RotationAxis(axis, -0.40).toEulerAngles()
+    });
+    moveLeftLegDown_keys.push({
+        frame: 8 * frameRate,
+        value: new BABYLON.Quaternion.RotationAxis(axis, 0.00).toEulerAngles()
+    });
+
+    var moveLeftLegDown = new BABYLON.Animation("moveLeftLegDown", "rotation", 5, BABYLON.Animation.ANIMATIONTYPE_VECTOR3, BABYLON.Animation.ANIMATIONLOOPMODE_CYCLE);
+    var moveRightLegDown = new BABYLON.Animation("moveRightLegDown", "rotation", 5, BABYLON.Animation.ANIMATIONTYPE_VECTOR3, BABYLON.Animation.ANIMATIONLOOPMODE_CYCLE);
+
+    var moveLeftArm_keys = [];
+
+    moveLeftArm_keys.push({
+        frame: 0,
+        value: new BABYLON.Quaternion.RotationAxis(axis, 0.90).toEulerAngles()
+    });
+
+    moveLeftArm_keys.push({
+        frame: 1 * frameRate,
+        value: new BABYLON.Quaternion.RotationAxis(axis, 0.40).toEulerAngles()
+    });
+
+    moveLeftArm_keys.push({
+        frame: 2 * frameRate,
+        value: new BABYLON.Quaternion.RotationAxis(axis, 0.20).toEulerAngles()
+    });
+
+    moveLeftArm_keys.push({
+        frame: 3 * frameRate,
+        value: new BABYLON.Quaternion.RotationAxis(axis, 0.00).toEulerAngles()
+    });
+
+    moveLeftArm_keys.push({
+        frame: 4 * frameRate,
+        value: new BABYLON.Quaternion.RotationAxis(axis, -0.40).toEulerAngles()
+    });
+
+    moveLeftArm_keys.push({
+        frame: 5 * frameRate,
+        value: new BABYLON.Quaternion.RotationAxis(axis, -0.20).toEulerAngles()
+    });
+
+    moveLeftArm_keys.push({
+        frame: 6 * frameRate,
+        value: new BABYLON.Quaternion.RotationAxis(axis, 0.20).toEulerAngles()
+    });
+
+    moveLeftArm_keys.push({
+        frame: 7 * frameRate,
+        value: new BABYLON.Quaternion.RotationAxis(axis, 0.40).toEulerAngles()
+    });
+    moveLeftArm_keys.push({
+        frame: 8 * frameRate,
+        value: new BABYLON.Quaternion.RotationAxis(axis, 0.90).toEulerAngles()
+    });
+    var moveLeftLeg = new BABYLON.Animation("moveLeftLeg", "rotation", 5, BABYLON.Animation.ANIMATIONTYPE_VECTOR3, BABYLON.Animation.ANIMATIONLOOPMODE_CYCLE);
+
+    var moveRightArm_keys = [];
+    moveRightArm_keys.push({
+        frame: 0 * frameRate,
+        value: new BABYLON.Quaternion.RotationAxis(axis, -0.40).toEulerAngles()
+    });
+
+    moveRightArm_keys.push({
+        frame: 1 * frameRate,
+        value: new BABYLON.Quaternion.RotationAxis(axis, -0.20).toEulerAngles()
+    });
+
+    moveRightArm_keys.push({
+        frame: 2 * frameRate,
+        value: new BABYLON.Quaternion.RotationAxis(axis, 0.20).toEulerAngles()
+    });
+
+    moveRightArm_keys.push({
+        frame: 3 * frameRate,
+        value: new BABYLON.Quaternion.RotationAxis(axis, 0.40).toEulerAngles()
+    });
+
+    moveRightArm_keys.push({
+        frame: 4 * frameRate,
+        value: new BABYLON.Quaternion.RotationAxis(axis, 0.90).toEulerAngles()
+    });
+
+    moveRightArm_keys.push({
+        frame: 5 * frameRate,
+        value: new BABYLON.Quaternion.RotationAxis(axis, 0.40).toEulerAngles()
+    });
+
+    moveRightArm_keys.push({
+        frame: 6 * frameRate,
+        value: new BABYLON.Quaternion.RotationAxis(axis, 0.20).toEulerAngles()
+    });
+
+    moveRightArm_keys.push({
+        frame: 7 * frameRate,
+        value: new BABYLON.Quaternion.RotationAxis(axis, 0.00).toEulerAngles()
+    });
+
+    moveRightArm_keys.push({
+        frame: 8 * frameRate,
+        value: new BABYLON.Quaternion.RotationAxis(axis, -0.40).toEulerAngles()
+    });
+    var moveLeftArm = new BABYLON.Animation("moveLeftArm_keys", "rotation", 5, BABYLON.Animation.ANIMATIONTYPE_VECTOR3, BABYLON.Animation.ANIMATIONLOOPMODE_CYCLE);
+    var moveRightArm = new BABYLON.Animation("moveRightArm_keys", "rotation", 5, BABYLON.Animation.ANIMATIONTYPE_VECTOR3, BABYLON.Animation.ANIMATIONLOOPMODE_CYCLE);
+
+
+
+
+
+    var moveLeftForearm_keys = [];
+
+    moveLeftForearm_keys.push({
+        frame: 0,
+        value: new BABYLON.Quaternion.RotationAxis(axis, 0.70).toEulerAngles()
+    });
+
+    moveLeftForearm_keys.push({
+        frame: 1 * frameRate,
+        value: new BABYLON.Quaternion.RotationAxis(axis, 0.60).toEulerAngles()
+    });
+
+    moveLeftForearm_keys.push({
+        frame: 2 * frameRate,
+        value: new BABYLON.Quaternion.RotationAxis(axis, 0.40).toEulerAngles()
+    });
+
+    moveLeftForearm_keys.push({
+        frame: 3 * frameRate,
+        value: new BABYLON.Quaternion.RotationAxis(axis, 0.00).toEulerAngles()
+    });
+
+    moveLeftForearm_keys.push({
+        frame: 4 * frameRate,
+        value: new BABYLON.Quaternion.RotationAxis(axis, 0.40).toEulerAngles()
+    });
+
+    moveLeftForearm_keys.push({
+        frame: 5 * frameRate,
+        value: new BABYLON.Quaternion.RotationAxis(axis, 0.60).toEulerAngles()
+    });
+
+    moveLeftForearm_keys.push({
+        frame: 6 * frameRate,
+        value: new BABYLON.Quaternion.RotationAxis(axis, 0.70).toEulerAngles()
+    });
+
+    moveLeftForearm_keys.push({
+        frame: 7 * frameRate,
+        value: new BABYLON.Quaternion.RotationAxis(axis, 1.00).toEulerAngles()
+    });
+    moveLeftForearm_keys.push({
+        frame: 8 * frameRate,
+        value: new BABYLON.Quaternion.RotationAxis(axis, 0.70).toEulerAngles()
+    });
+
+
+    var moveRightForearm_keys = [];
+    moveRightForearm_keys.push({
+        frame: 0 * frameRate,
+        value: new BABYLON.Quaternion.RotationAxis(axis, 0.00).toEulerAngles()
+    });
+
+    moveRightForearm_keys.push({
+        frame: 1 * frameRate,
+        value: new BABYLON.Quaternion.RotationAxis(axis, 0.40).toEulerAngles()
+    });
+
+    moveRightForearm_keys.push({
+        frame: 2 * frameRate,
+        value: new BABYLON.Quaternion.RotationAxis(axis, 0.60).toEulerAngles()
+    });
+
+    moveRightForearm_keys.push({
+        frame: 3 * frameRate,
+        value: new BABYLON.Quaternion.RotationAxis(axis, 0.70).toEulerAngles()
+    });
+
+    moveRightForearm_keys.push({
+        frame: 4 * frameRate,
+        value: new BABYLON.Quaternion.RotationAxis(axis, 1.00).toEulerAngles()
+    });
+
+    moveRightForearm_keys.push({
+        frame: 5 * frameRate,
+        value: new BABYLON.Quaternion.RotationAxis(axis, 0.70).toEulerAngles()
+    });
+
+    moveRightForearm_keys.push({
+        frame: 6 * frameRate,
+        value: new BABYLON.Quaternion.RotationAxis(axis, 0.60).toEulerAngles()
+    });
+
+    moveRightForearm_keys.push({
+        frame: 7 * frameRate,
+        value: new BABYLON.Quaternion.RotationAxis(axis, 0.40).toEulerAngles()
+    });
+    moveRightForearm_keys.push({
+        frame: 8 * frameRate,
+        value: new BABYLON.Quaternion.RotationAxis(axis, 0.00).toEulerAngles()
+    });
+
+
+    var moveLeftForearm = new BABYLON.Animation("moveLeftForearm_keys", "rotation", 5, BABYLON.Animation.ANIMATIONTYPE_VECTOR3, BABYLON.Animation.ANIMATIONLOOPMODE_CYCLE);
+    var moveRightForearm = new BABYLON.Animation("moveRightForearm_keys", "rotation", 5, BABYLON.Animation.ANIMATIONTYPE_VECTOR3, BABYLON.Animation.ANIMATIONLOOPMODE_CYCLE);
+
+    var moveLeftFoot_keys = [];
+    moveLeftFoot_keys.push({
+        frame: 0 * frameRate,
+        value: new BABYLON.Quaternion.RotationAxis(axis, -0.10).toEulerAngles()
+    });
+
+    moveLeftFoot_keys.push({
+        frame: 1 * frameRate,
+        value: new BABYLON.Quaternion.RotationAxis(axis, -0.30).toEulerAngles()
+    });
+
+    moveLeftFoot_keys.push({
+        frame: 2 * frameRate,
+        value: new BABYLON.Quaternion.RotationAxis(axis, -0.20).toEulerAngles()
+    });
+
+    moveLeftFoot_keys.push({
+        frame: 3 * frameRate,
+        value: new BABYLON.Quaternion.RotationAxis(axis, 0.00).toEulerAngles()
+    });
+
+    moveLeftFoot_keys.push({
+        frame: 4 * frameRate,
+        value: new BABYLON.Quaternion.RotationAxis(axis, 0.10).toEulerAngles()
+    });
+
+    moveLeftFoot_keys.push({
+        frame: 5 * frameRate,
+        value: new BABYLON.Quaternion.RotationAxis(axis, 0.40).toEulerAngles()
+    });
+
+    moveLeftFoot_keys.push({
+        frame: 6 * frameRate,
+        value: new BABYLON.Quaternion.RotationAxis(axis, 0.10).toEulerAngles()
+    });
+
+    moveLeftFoot_keys.push({
+        frame: 7 * frameRate,
+        value: new BABYLON.Quaternion.RotationAxis(axis, 0.00).toEulerAngles()
+    });
+    moveLeftFoot_keys.push({
+        frame: 8 * frameRate,
+        value: new BABYLON.Quaternion.RotationAxis(axis, -0.10).toEulerAngles()
+    });
+
+    var moveRightFoot_keys = [];
+    moveRightFoot_keys.push({
+        frame: 0 * frameRate,
+        value: new BABYLON.Quaternion.RotationAxis(axis, 0.40).toEulerAngles()
+    });
+
+    moveRightFoot_keys.push({
+        frame: 1 * frameRate,
+        value: new BABYLON.Quaternion.RotationAxis(axis, 0.10).toEulerAngles()
+    });
+
+    moveRightFoot_keys.push({
+        frame: 2 * frameRate,
+        value: new BABYLON.Quaternion.RotationAxis(axis, 0.00).toEulerAngles()
+    });
+
+    moveRightFoot_keys.push({
+        frame: 3 * frameRate,
+        value: new BABYLON.Quaternion.RotationAxis(axis, -0.10).toEulerAngles()
+    });
+
+    moveRightFoot_keys.push({
+        frame: 4 * frameRate,
+        value: new BABYLON.Quaternion.RotationAxis(axis, -0.30).toEulerAngles()
+    });
+
+    moveRightFoot_keys.push({
+        frame: 5 * frameRate,
+        value: new BABYLON.Quaternion.RotationAxis(axis, -0.20).toEulerAngles()
+    });
+
+    moveRightFoot_keys.push({
+        frame: 6 * frameRate,
+        value: new BABYLON.Quaternion.RotationAxis(axis, 0.00).toEulerAngles()
+    });
+
+    moveRightFoot_keys.push({
+        frame: 7 * frameRate,
+        value: new BABYLON.Quaternion.RotationAxis(axis, 0.10).toEulerAngles()
+    });
+    moveRightFoot_keys.push({
+        frame: 8 * frameRate,
+        value: new BABYLON.Quaternion.RotationAxis(axis, 0.40).toEulerAngles()
+    });
+
+    var moveLeftFoot = new BABYLON.Animation("moveLeftFoot_keys", "rotation", 5, BABYLON.Animation.ANIMATIONTYPE_VECTOR3, BABYLON.Animation.ANIMATIONLOOPMODE_CYCLE);
+    var moveRightFoot = new BABYLON.Animation("moveRightFoot_keys", "rotation", 5, BABYLON.Animation.ANIMATIONTYPE_VECTOR3, BABYLON.Animation.ANIMATIONLOOPMODE_CYCLE);
+
+
+
+
 
     console.log(new BABYLON.Quaternion.RotationAxis(axis, Math.PI/2).toEulerAngles());
     movein.setKeys(moveRightLeg_keys);
     moveLeftLeg.setKeys(moveLeftLeg_keys);
+    moveLeftLegDown.setKeys(moveLeftLegDown_keys);
+    moveRightLegDown.setKeys(moveRightLegDown_keys);
+    moveLeftArm.setKeys(moveLeftArm_keys);
+    moveRightArm.setKeys(moveRightArm_keys);
+    moveLeftForearm.setKeys(moveLeftForearm_keys);
+    moveRightForearm.setKeys(moveRightForearm_keys);
+    moveLeftFoot.setKeys(moveLeftFoot_keys);
+    moveRightFoot.setKeys(moveRightFoot_keys);
 
     var animationGroupWalk = new BABYLON.AnimationGroup("Walk");
+
     animationGroupWalk.addTargetedAnimation(movein, character.rt_upperleg);
     animationGroupWalk.addTargetedAnimation(moveLeftLeg, character.lt_upperleg);
+    animationGroupWalk.addTargetedAnimation(moveLeftLegDown, character.lt_downleg);
+    animationGroupWalk.addTargetedAnimation(moveRightLegDown, character.rt_downleg);
+    animationGroupWalk.addTargetedAnimation(moveLeftArm, character.lt_arm);
+    animationGroupWalk.addTargetedAnimation(moveRightArm, character.rt_arm);
+    animationGroupWalk.addTargetedAnimation(moveLeftForearm, character.lt_forearm);
+    animationGroupWalk.addTargetedAnimation(moveRightForearm, character.rt_forearm);
+    animationGroupWalk.addTargetedAnimation(moveLeftFoot, character.lt_foot);
+    animationGroupWalk.addTargetedAnimation(moveRightFoot, character.rt_foot);
     animationGroupWalk.play(true);
 }
